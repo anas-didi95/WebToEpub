56bed6ff49355d793bd84b1b9554bf122575bc1a
Anas Juwaidi <anas.didi95@tutamail.com>
2026-02-18 04:34:05 +0000

    MOD: Fix lint

--- plugin/js/parsers/LittlePandaParser.js
+++ plugin/js/parsers/LittlePandaParser.js
@@ -12,7 +12,7 @@ class LittlePandaParser extends Parser { // eslint-disable-line no-unused-vars
 
     // returns promise with the URLs of the chapters to fetch
     // promise is used because may need to fetch the list of URLs from internet
-    async getChapterUrls(dom, chapterUrlsUI) {
+    async getChapterUrls(dom) {
         const menu = dom.querySelector("table.posts-table") ;
         return util.hyperlinksToChapterList(menu);
     }
