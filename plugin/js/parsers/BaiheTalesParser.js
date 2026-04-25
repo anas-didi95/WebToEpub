dfe762b1b27d840dfa5bbad5cd6efce4d3828bdc
Anas Juwaidi <anas.didi95@tutamail.com>
2026-04-25 18:01:22 +0000

    add: implement parser

--- plugin/js/parsers/BaiheTalesParser.js
+++ plugin/js/parsers/BaiheTalesParser.js
@@ -1,104 +1,39 @@
-/*
-  Template to use to create a new parser
-*/
 "use strict";
 
-// Use one or more of these to specify when the parser is to be used
-/*
-// Use this function if site's host name is sufficient.  
-// i.e. All pages are on same site, and use same format.
-parserFactory.register("template.org", () => new TemplateParser());
+parserFactory.register("baihetales.wordpress.com", () => new BaiheTalesParser());
 
-// Use this function if site's URL is sufficient
-parserFactory.registerUrlRule(
-    url => TemplateParser.urlMeetsSelectionCriteria(url), 
-    () => new TemplateParser()
-);
-
-// Use this if pages are on multiple sites, or host name isn't unique
-parserFactory.registerRule(
-    function(url, dom) {
-        return TemplateParser.urlMeetsSelectionCriteria(url) ||
-            TemplateParser.domMeetsSelectionCritera(dom); 
-    }, 
-    () => new TemplateParser()
-);
-*/
-
-class TemplateParser extends Parser { // eslint-disable-line no-unused-vars
+class BaiheTalesParser extends Parser { // eslint-disable-line no-unused-vars
     constructor() {
         super();
-        //Optional Parameters:
-
-        /*
-        // Minimum delay (in ms) between page requests. Useful for 403 error prevention.
-        // If the sites this parser accesses throttles requests or uses cloudflare, it is recommended to set this.
-        this.minimumThrottle = 3000;
-        */
     }
 
     // returns promise with the URLs of the chapters to fetch
     // promise is used because may need to fetch the list of URLs from internet
-    /*
     async getChapterUrls(dom, chapterUrlsUI) {
-        // Most common implementation is to find element holding the hyperlinks to 
-        // the web pages holding the chapters.  Then call util.hyperlinksToChapterList()
-        // to convert the links into a list of URLs the parser will collect.
-        let menu = dom.querySelector("div.su-tabs-panes");
-        return util.hyperlinksToChapterList(menu);
+        const chaptersFromDom = (dom) => {
+            const urls = dom.querySelector("ul.wp-block-post-template");
+            return util.hyperlinksToChapterList(urls);
+        };        
+        const nextTocPageUrl = (dom) => dom.querySelector("nav a.wp-block-query-pagination-next")?.href;
 
-        // Almost as common, find links on page and convert.
-        return [...dom.querySelectorAll("li.wp-manga-chapter.free-chap a")]
-            .map(a => util.hyperLinkToChapter(a));
-
-        // Need to walk multiple ToC pages, page by page
-        return (await this.walkTocPages(dom, 
-            TemplateParser.chaptersFromDom, 
-            TemplateParser.nextTocPageUrl, 
-            chapterUrlsUI
-        ));
-
-        // Can get list of all ToC pages
-        let tocPage1chapters = TemplateParser.extractPartialChapterList(dom);
-        let urlsOfTocPages  = TemplateParser.getUrlsOfTocPages(dom);
-        return (await this.getChaptersFromAllTocPages(tocPage1chapters,
-            TemplateParser.extractPartialChapterList,
-            urlsOfTocPages,
-            chapterUrlsUI
-        ));
+        return (await this.walkTocPages(dom, chaptersFromDom, nextTocPageUrl, chapterUrlsUI));
     }
-    */
 
     // returns the element holding the story content in a chapter
-    /*
     findContent(dom) {
-        // typical implementation is find node with all wanted content
-        // return is the element holding just the wanted content.
-        return dom.querySelector("article");
+        return dom.querySelector("article.post");
     }
-    */
 
     // title of the story  (not to be confused with title of each chapter)
-    /*
     extractTitleImpl(dom) {
-        // typical implementation is find node with the Title and return name from title
-        // NOTE. Can return Title as a string, or an  HTML element
-        return dom.querySelector("h1");
+        return dom.querySelector("h1.entry-title").textContent.trim();
     }
-    */
 
     // author of the story
-    // Optional, if not provided, will default to "<unknown>"
-    /*
     extractAuthor(dom) {
-        // typical implementation is find node with the author's name and return name from title
-        // Major points to note
-        //   1. Return the Author's name as a string, not a HTML element
-        //   2. If can't find Author, call the base implementation
-        let authorLabel = dom.querySelector(".meta span a");
-        return authorLabel?.textContent ?? super.extractAuthor(dom);
+        const label = dom.querySelector(".wp-block-media-text__content > p:nth-child(2)");
+        return label?.textContent.replace("Author:", "").trim() ?? super.extractAuthor(dom);
     }
-    */
 
     // language used
     // Optional, if not provided, will default to ISO code for English "en"
@@ -130,11 +65,10 @@ class TemplateParser extends Parser { // eslint-disable-line no-unused-vars
 
     // Description of the story
     // Optional, Description for metadata, if not provided, will default to ""
-    /*
     extractDescription(dom) {
-        return dom.querySelector("div [property='description']").textContent.trim();
+        return Array.from(dom.querySelectorAll("div.entry-content p.wp-block-paragraph"))
+            .map(a => a.textContent.trim()).join("\n") ?? super.extractDescription(dom);
     }
-    */
 
     // Optional, supply if need to do special manipulation of content
     // e.g. decrypt content
@@ -145,21 +79,20 @@ class TemplateParser extends Parser { // eslint-disable-line no-unused-vars
     */
 
     // Optional, supply if need to do custom cleanup of content
-    /*
     removeUnwantedElementsFromContentElement(element) {
-        util.removeChildElementsMatchingSelector(element, "button");
+        const end = element.querySelector("hr");
+        while (end.nextElementSibling) {
+            end.nextElementSibling.remove();
+        }
+        end.remove();
+        util.removeChildElementsMatchingSelector(element, "footer");
         super.removeUnwantedElementsFromContentElement(element);
     }
-    */
 
     // Optional, supply if individual chapter titles are not inside the content element
-    /*
     findChapterTitle(dom) {
-        // typical implementation is find node with the Title
-        // Return Title element, OR the title as a string
-        return dom.querySelector("h3.dashhead-title");
+        return dom.querySelector("h1.entry-title");
     }
-    */
 
     // Optional, if "next/previous chapter" are nested inside other elements,
     // this says how to find the highest parent element to remove
@@ -178,12 +111,9 @@ class TemplateParser extends Parser { // eslint-disable-line no-unused-vars
     // Optional, supply if cover image can usually be found on inital web page
     // Notes.
     //   1. If cover image is first image in content section, do not implement this function
-    /*
     findCoverImageUrl(dom) {
-        // Most common implementation is get first image in specified container. e.g. 
-        return util.getFirstImgSrc(dom, "div.td-ss-main-sidebar");
+        return util.getFirstImgSrc(dom, "figure");
     }
-    */
 
     // Optional, supply if need to chase hyperlinks in page to get all chapter content
     // or site can send challenge pages for some chapters
@@ -233,11 +163,10 @@ class TemplateParser extends Parser { // eslint-disable-line no-unused-vars
 
     // Optional, Return elements from page
     // that are to be shown on epub's "information" page
-    /*
     getInformationEpubItemChildNodes(dom) {
-        return [...dom.querySelectorAll("div.novel-details")];
+        const contents = Array.from(dom.querySelectorAll("div.entry-content p.wp-block-paragraph"));
+        return contents.slice(0, contents.length - 1);
     }
-    */
 
     // Optional, Any cleanup operations to perform on the nodes
     // returned by getInformationEpubItemChildNodes
