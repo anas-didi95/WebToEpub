306310e296f8521e7a15e69637b7d1b7b274256c
Anas Juwaidi <anas.didi95@tutamail.com>
2026-04-26 08:56:58 +0000

    add: implement parser

--- plugin/js/parsers/LovingBaiheParser.js
+++ plugin/js/parsers/LovingBaiheParser.js
@@ -5,64 +5,31 @@ parserFactory.register("love4baihe.blogspot.com", () => new LovingBaiheParser())
 class LovingBaiheParser extends Parser { // eslint-disable-line no-unused-vars
     constructor() {
         super();
-        //Optional Parameters:
-
-        /*
-        // Minimum delay (in ms) between page requests. Useful for 403 error prevention.
-        // If the sites this parser accesses throttles requests or uses cloudflare, it is recommended to set this.
-        this.minimumThrottle = 3000;
-        */
     }
 
     // returns promise with the URLs of the chapters to fetch
     // promise is used because may need to fetch the list of URLs from internet
-    /*
     async getChapterUrls(dom, chapterUrlsUI) {
-        // Most common implementation is to find element holding the hyperlinks to 
-        // the web pages holding the chapters.  Then call util.hyperlinksToChapterList()
-        // to convert the links into a list of URLs the parser will collect.
-        let menu = dom.querySelector("div.su-tabs-panes");
-        return util.hyperlinksToChapterList(menu);
+        const parseSourceUrl = (el) => el.querySelector("a").href;
+        const parseTitle = (el) => el.querySelector("span.chapternum").innerText.trim();
+        const links = Array.from(dom.querySelectorAll("div#chapters ul li.relative")).reverse();
 
-        // Almost as common, find links on page and convert.
-        return [...dom.querySelectorAll("li.wp-manga-chapter.free-chap a")]
-            .map(a => util.hyperLinkToChapter(a));
-
-        // Need to walk multiple ToC pages, page by page
-        return (await this.walkTocPages(dom, 
-            TemplateParser.chaptersFromDom, 
-            TemplateParser.nextTocPageUrl, 
-            chapterUrlsUI
-        ));
-
-        // Can get list of all ToC pages
-        let tocPage1chapters = TemplateParser.extractPartialChapterList(dom);
-        let urlsOfTocPages  = TemplateParser.getUrlsOfTocPages(dom);
-        return (await this.getChaptersFromAllTocPages(tocPage1chapters,
-            TemplateParser.extractPartialChapterList,
-            urlsOfTocPages,
-            chapterUrlsUI
-        ));
+        return links.map(el => ({
+            sourceUrl: parseSourceUrl(el),
+            title: parseTitle(el),
+            newArc: null
+        }));
     }
-    */
 
     // returns the element holding the story content in a chapter
-    /*
     findContent(dom) {
-        // typical implementation is find node with all wanted content
-        // return is the element holding just the wanted content.
         return dom.querySelector("article");
     }
-    */
 
     // title of the story  (not to be confused with title of each chapter)
-    /*
     extractTitleImpl(dom) {
-        // typical implementation is find node with the Title and return name from title
-        // NOTE. Can return Title as a string, or an  HTML element
-        return dom.querySelector("h1");
+        return dom.querySelector("h1[itemprop='name']");
     }
-    */
 
     // author of the story
     // Optional, if not provided, will default to "<unknown>"
@@ -107,11 +74,9 @@ class LovingBaiheParser extends Parser { // eslint-disable-line no-unused-vars
 
     // Description of the story
     // Optional, Description for metadata, if not provided, will default to ""
-    /*
     extractDescription(dom) {
-        return dom.querySelector("div [property='description']").textContent.trim();
+        return dom.querySelector("div#syn-target").innerText.trim() ?? super.extractDescription(dom);
     }
-    */
 
     // Optional, supply if need to do special manipulation of content
     // e.g. decrypt content
@@ -122,21 +87,16 @@ class LovingBaiheParser extends Parser { // eslint-disable-line no-unused-vars
     */
 
     // Optional, supply if need to do custom cleanup of content
-    /*
     removeUnwantedElementsFromContentElement(element) {
-        util.removeChildElementsMatchingSelector(element, "button");
+        element.querySelector("img")?.remove();
+        util.removeChildElementsMatchingSelector(element, "h2");
         super.removeUnwantedElementsFromContentElement(element);
     }
-    */
 
     // Optional, supply if individual chapter titles are not inside the content element
-    /*
     findChapterTitle(dom) {
-        // typical implementation is find node with the Title
-        // Return Title element, OR the title as a string
-        return dom.querySelector("h3.dashhead-title");
+        return dom.querySelector("article span b");
     }
-    */
 
     // Optional, if "next/previous chapter" are nested inside other elements,
     // this says how to find the highest parent element to remove
@@ -155,12 +115,9 @@ class LovingBaiheParser extends Parser { // eslint-disable-line no-unused-vars
     // Optional, supply if cover image can usually be found on inital web page
     // Notes.
     //   1. If cover image is first image in content section, do not implement this function
-    /*
     findCoverImageUrl(dom) {
-        // Most common implementation is get first image in specified container. e.g. 
-        return util.getFirstImgSrc(dom, "div.td-ss-main-sidebar");
+        return util.getFirstImgSrc(dom, "header");
     }
-    */
 
     // Optional, supply if need to chase hyperlinks in page to get all chapter content
     // or site can send challenge pages for some chapters
@@ -210,11 +167,9 @@ class LovingBaiheParser extends Parser { // eslint-disable-line no-unused-vars
 
     // Optional, Return elements from page
     // that are to be shown on epub's "information" page
-    /*
     getInformationEpubItemChildNodes(dom) {
-        return [...dom.querySelectorAll("div.novel-details")];
+        return [dom.querySelector("div#syn-target")];
     }
-    */
 
     // Optional, Any cleanup operations to perform on the nodes
     // returned by getInformationEpubItemChildNodes
