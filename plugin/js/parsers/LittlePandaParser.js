947046fe0ecf1634e3549fc6c9414cf924a38768
Anas Juwaidi <anas.didi95@tutamail.com>
2026-05-01 06:16:48 +0000

    add: implement chapter

--- plugin/js/parsers/LittlePandaParser.js
+++ plugin/js/parsers/LittlePandaParser.js
@@ -5,55 +5,19 @@ parserFactory.register("littlepandatranslations.com", () => new LittlePandaParse
 class LittlePandaParser extends Parser { // eslint-disable-line no-unused-vars
     constructor() {
         super();
-        //Optional Parameters:
-
-        /*
-        // Minimum delay (in ms) between page requests. Useful for 403 error prevention.
-        // If the sites this parser accesses throttles requests or uses cloudflare, it is recommended to set this.
-        this.minimumThrottle = 3000;
-        */
     }
 
     // returns promise with the URLs of the chapters to fetch
     // promise is used because may need to fetch the list of URLs from internet
-    /*
-    async getChapterUrls(dom, chapterUrlsUI) {
-        // Most common implementation is to find element holding the hyperlinks to 
-        // the web pages holding the chapters.  Then call util.hyperlinksToChapterList()
-        // to convert the links into a list of URLs the parser will collect.
-        let menu = dom.querySelector("div.su-tabs-panes");
+    async getChapterUrls(dom) {
+        let menu = dom.querySelector("ol.chapter-group__list");
         return util.hyperlinksToChapterList(menu);
-
-        // Almost as common, find links on page and convert.
-        return [...dom.querySelectorAll("li.wp-manga-chapter.free-chap a")]
-            .map(a => util.hyperLinkToChapter(a));
-
-        // Need to walk multiple ToC pages, page by page
-        return (await this.walkTocPages(dom, 
-            TemplateParser.chaptersFromDom, 
-            TemplateParser.nextTocPageUrl, 
-            chapterUrlsUI
-        ));
-
-        // Can get list of all ToC pages
-        let tocPage1chapters = TemplateParser.extractPartialChapterList(dom);
-        let urlsOfTocPages  = TemplateParser.getUrlsOfTocPages(dom);
-        return (await this.getChaptersFromAllTocPages(tocPage1chapters,
-            TemplateParser.extractPartialChapterList,
-            urlsOfTocPages,
-            chapterUrlsUI
-        ));
     }
-    */
 
     // returns the element holding the story content in a chapter
-    /*
     findContent(dom) {
-        // typical implementation is find node with all wanted content
-        // return is the element holding just the wanted content.
-        return dom.querySelector("article");
+        return dom.querySelector("section#chapter-content");
     }
-    */
 
     // title of the story  (not to be confused with title of each chapter)
     /*
@@ -155,12 +119,9 @@ class LittlePandaParser extends Parser { // eslint-disable-line no-unused-vars
     // Optional, supply if cover image can usually be found on inital web page
     // Notes.
     //   1. If cover image is first image in content section, do not implement this function
-    /*
     findCoverImageUrl(dom) {
-        // Most common implementation is get first image in specified container. e.g. 
-        return util.getFirstImgSrc(dom, "div.td-ss-main-sidebar");
+        return util.getFirstImgSrc(dom, "figure.story__thumbnail");
     }
-    */
 
     // Optional, supply if need to chase hyperlinks in page to get all chapter content
     // or site can send challenge pages for some chapters
