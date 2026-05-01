995506f2c2e7cf42950116228c9f3e75bc79731f
Anas Juwaidi <anas.didi95@tutamail.com>
2026-05-01 13:36:37 +0000

    add: implement get chapter title

--- plugin/js/parsers/LovelyblossomsParser.js
+++ plugin/js/parsers/LovelyblossomsParser.js
@@ -93,13 +93,9 @@ class LovelyblossomsParser extends Parser { // eslint-disable-line no-unused-var
     }
 
     // Optional, supply if individual chapter titles are not inside the content element
-    /*
     findChapterTitle(dom) {
-        // typical implementation is find node with the Title
-        // Return Title element, OR the title as a string
-        return dom.querySelector("h3.dashhead-title");
+        return dom.querySelector("h1#chapter-heading");
     }
-    */
 
     // Optional, if "next/previous chapter" are nested inside other elements,
     // this says how to find the highest parent element to remove
