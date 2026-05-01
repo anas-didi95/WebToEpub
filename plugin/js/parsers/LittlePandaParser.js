e2af436a3b19040924bb3174b5cca75a04ab7612
Anas Juwaidi <anas.didi95@tutamail.com>
2026-05-01 06:40:44 +0000

    rem: remove used comments

--- plugin/js/parsers/LittlePandaParser.js
+++ plugin/js/parsers/LittlePandaParser.js
@@ -19,72 +19,6 @@ class LittlePandaParser extends Parser { // eslint-disable-line no-unused-vars
         return dom.querySelector("section#chapter-content");
     }
 
-    // title of the story  (not to be confused with title of each chapter)
-    /*
-    extractTitleImpl(dom) {
-        // typical implementation is find node with the Title and return name from title
-        // NOTE. Can return Title as a string, or an  HTML element
-        return dom.querySelector("h1");
-    }
-    */
-
-    // author of the story
-    // Optional, if not provided, will default to "<unknown>"
-    /*
-    extractAuthor(dom) {
-        // typical implementation is find node with the author's name and return name from title
-        // Major points to note
-        //   1. Return the Author's name as a string, not a HTML element
-        //   2. If can't find Author, call the base implementation
-        let authorLabel = dom.querySelector(".meta span a");
-        return authorLabel?.textContent ?? super.extractAuthor(dom);
-    }
-    */
-
-    // language used
-    // Optional, if not provided, will default to ISO code for English "en"
-    /*
-    extractLanguage(dom) {
-        return dom.querySelector("html").getAttribute("lang");
-    }
-    */
-
-    // load EpubMetaInfo async in local variable to retieve with all other Metadata functions
-    // Optional, will default to "return"
-    /*
-    async loadEpubMetaInfo(){
-        let data = (await HttpClient.fetchJson(api)).json;
-        this.subject = data.subject;
-        ...
-        return;
-    }
-    */
-
-    // Genre of the story
-    // Optional, Genre for metadata, if not provided, will default to ""
-    /*
-    extractSubject(dom) {
-        let tags = [...dom.querySelectorAll("[property='genre']")];
-        return tags.map(e => e.textContent.trim()).join(", ");
-    }
-    */
-
-    // Description of the story
-    // Optional, Description for metadata, if not provided, will default to ""
-    /*
-    extractDescription(dom) {
-        return dom.querySelector("div [property='description']").textContent.trim();
-    }
-    */
-
-    // Optional, supply if need to do special manipulation of content
-    // e.g. decrypt content
-    /*
-    customRawDomToContentStep(chapter, content) {
-        // for example of this, refer to LnmtlParser
-    }
-    */
-
     // Optional, supply if need to do custom cleanup of content
     removeUnwantedElementsFromContentElement(element) {
         util.removeChildElementsMatchingSelector(element, "span.ts-honeypot");
@@ -98,20 +32,6 @@ class LittlePandaParser extends Parser { // eslint-disable-line no-unused-vars
         return dom.querySelector("h1.chapter__title");
     }
 
-    // Optional, if "next/previous chapter" are nested inside other elements,
-    // this says how to find the highest parent element to remove
-    /*
-    findParentNodeOfChapterLinkToRemoveAt(link) {
-        // The links may be wrapped, so need to walk up tree to find the 
-        // highest element holding the chapter links.
-        // e.g. Following code assumes links are sometimes enclosed in a <strong> tag
-        // that is enclosed in a <p> tag.  We want to remove the <p> tag
-        // and everything inside it
-        let toRemove = util.moveIfParent(link, "strong");
-        return util.moveIfParent(toRemove, "p");    
-    }
-    */
-
     // Optional, supply if cover image can usually be found on inital web page
     // Notes.
     //   1. If cover image is first image in content section, do not implement this function
@@ -119,64 +39,10 @@ class LittlePandaParser extends Parser { // eslint-disable-line no-unused-vars
         return util.getFirstImgSrc(dom, "figure.story__thumbnail");
     }
 
-    // Optional, supply if need to chase hyperlinks in page to get all chapter content
-    // or site can send challenge pages for some chapters
-    /*
-    async fetchChapter(url) {
-        return (await HttpClient.wrapFetch(url)).responseXML;
-
-        // Handling to catch sites that send challenge pages
-        // Note, need to implement isCustomError() and setCustomErrorResponse()
-        let options = { parser: this };
-        return (await HttpClient.wrapFetch(url, options)).responseXML;
-    }
-    */
-
-    // Optional, supply these if site can send challenge pages for some chapters
-    /*
-    // return true if response is a challenge response
-    isCustomError(response){
-        return (response.responseXML.title == "Just a moment...");
-    }
-
-    // what to do if encounter challenge
-    setCustomErrorResponse(url, wrapOptions){
-        let newresp = {};
-        newresp.url = url;
-        newresp.wrapOptions = wrapOptions;
-        newresp.response = {};
-        newresp.response.url = this.RestToUrl(checkedresponse.response.url);
-        newresp.response.status = 403;
-        return newresp;
-    }
-    */
-
-    // Optional, supply if need to modify DOM before normal processing steps
-    /*
-    preprocessRawDom(webPageDom) {
-    }
-    */
-
-    // Optional, called when user presses the "Pack EPUB" button.
-    // Implement if parser needs to do anything after user sets UI settings 
-    // but before collecting pages
-    /*
-    onStartCollecting() {
-    }
-    */
-
     // Optional, Return elements from page
     // that are to be shown on epub's "information" page
     getInformationEpubItemChildNodes(dom) {
         const summary = dom.querySelector("section.story__summary");
         return [...summary.querySelectorAll("p")];
     }
-
-    // Optional, Any cleanup operations to perform on the nodes
-    // returned by getInformationEpubItemChildNodes
-    /*
-    cleanInformationNode(node) {
-        return node;
-    }
-    */
 }
